dc33d5e4c781f989c8190c3e3ed8e0f0ea04d475
soso chekurishvili <166957067+kynedasavleti@users.noreply.github.com>
2024-05-30 18:03:49 +0400

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,33 @@
+name = 'soso'
+
+proffesion ='goa student'
+
+last_name = 'chekurishvili'
+
+country = 'georgia'
+
+city = 'khasuri'
+
+hobby = 'workout'
+
+print(name , last_name , proffesion , hobby , country , city)
+
+print(360321 + 23124 )
+
+print(22313 - 12312)
+
+print(4672 * 12)
+
+print(99482 // 1221)
+
+print(46732 + 2123)
+
+print(12332 * 12432)
+
+print(1244023 - 12342)
+
+print(124432 // 12344)
+
+print(688930 + 1244423)
+
+print(4142432 * 345234)
